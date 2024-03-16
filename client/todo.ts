e4296e991ff9028316548a17f179b4701e34cc13
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-03-15 23:25:23 -0400

    Notes

--- client/todo.ts
+++ client/todo.ts
@@ -1,6 +1,8 @@
+// TODO: Sort by date for Work Orders
+// TODO: Get work order filters working
 // TODO: Style work order pages
+// TODO: Rewrite tabfilter logic it is messy
+
 // TODO: Work order creation is not enforcing the classroom type
 // TODO: isLoading states for all pages
 // TODO: Wrap entire app in safearea view?
-// TODO: Rewrite tabfilter logic it is messy
-// TODO: Sort by date for Work Orders
