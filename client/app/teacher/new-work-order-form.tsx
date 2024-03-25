b72afa57f3bc299ea15aaea27e6e02221e54570a
Shawn Ballay <shawnballay1@gmail.com>
2024-03-25 18:52:15 -0400

    Start NWorkOrderForm

--- client/app/teacher/new-work-order-form.tsx
+++ client/app/teacher/new-work-order-form.tsx
@@ -1,3 +1,15 @@
+import { View } from "@/components/Themed"
+import { KeyboardAvoidingView, Platform, TextInput } from "react-native"
+
 export default function NWorkOrderForm() {
-  return <></>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <View>
+        <TextInput></TextInput>
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
